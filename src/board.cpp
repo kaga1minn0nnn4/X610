cdb3d2ec8655e5b45cd586edac51e1a9018cad26
24266514 <s243270u@st.go.tuat.ac.jp>
2026-02-09 22:34:24 +0900

    ピン設定

---
+++ src/board.cpp
@@ -0,0 +1,84 @@
+#include "board.hpp"
+
+namespace x610 {
+
+G4::UART serial{G4::uart3, G4::PC10, G4::PC11};
+
+G4::TIM it_timer{G4::TimerClockParameter::generate<G4::tim2, kTimerInterruptFreq>()};
+G4::TIM pwm_timer{G4::TimerClockParameter::generate<G4::tim1, kPWMTimerFreq>()};
+
+std::array<G4::PWM, 3> pwms;
+
+std::array<G4::OpAMP, 3> opamps {
+    G4::OpAMP{G4::opamp1, G4::PA1},
+    G4::OpAMP{G4::opamp2, G4::PA7},
+    G4::OpAMP{G4::opamp3, G4::PB0},
+};
+
+std::array<G4::ADCv2, 2> adcs {
+    G4::ADCv2{G4::adc1, {G4::PB12, G4::PB11}},
+    G4::ADCv2{G4::adc2, {}}
+};
+
+G4::CANFD canfd{G4::canfd1, G4::PA12, G4::PA11};
+
+G4::GPIO n_fault{G4::PC15};
+G4::GPIO drvoff{G4::PC13};
+G4::LED user_led{G4::PB7};
+G4::PushSensor user_sw{G4::PB9};
+G4::PushSensor limit_sw{G4::PA0};
+
+std::vector<uint16_t> m2006_enc_values(2);
+std::vector<uint16_t> dummy;
+
+bool config() {
+    bool result = true;
+    result &= serial.config();
+    if (result) {
+        serial << "\n";
+        serial << "---------------------------\n";
+        serial << "X610 v1.0.0\n";
+        serial << "Start configuration !!!\n\n";
+    }
+
+    result &= drvoff.config(peripheral::gpio::Mode::out);
+    drvoff.write(true); // DRV8328 OFF
+
+    result &= n_fault.config(peripheral::gpio::Mode::in);
+    result &= user_led.config();
+    result &= user_sw.config();
+    result &= limit_sw.config();
+    serial << "GPIO init...\t" << result << "\n";
+
+    result &= G4::itTimerConfig(it_timer, 10, it_timer_task);
+    serial << "TIM2 init...\t" << result << "\n";
+
+    result &= pwm_timer.config(peripheral::tim::CounterMode::triangle, 1);
+    serial << "TIM1 init...\t" << result << "\n";
+
+    result &= pwm_timer.configOC(G4::TIM::Ch::_1, G4::TIM::PWMMode::complementary);
+    result &= pwm_timer.configOC(G4::TIM::Ch::_2, G4::TIM::PWMMode::complementary);
+    result &= pwm_timer.configOC(G4::TIM::Ch::_3, G4::TIM::PWMMode::complementary);
+    serial << "TIM1 Channel1~3 init...\t" << result << "\n";
+
+    result &= pwms[0].config(pwm_timer.createChannel(G4::TIM::Ch::_1), G4::PA8, G4::PB13);
+    result &= pwms[1].config(pwm_timer.createChannel(G4::TIM::Ch::_2), G4::PA9, G4::PB14);
+    result &= pwms[2].config(pwm_timer.createChannel(G4::TIM::Ch::_3), G4::PA10, G4::PB15);
+    serial << "PWM init...\t" << result << "\n";
+
+    for (auto& opamp : opamps) {
+        result &= opamp.config(peripheral::opamp::Mode::pga, peripheral::opamp::Gain::_2_or_minus_1);
+    }
+    serial << "OPAMP1~3 init...\t" << result << "\n";
+
+    result &= adcs[0].config({LL_ADC_CHANNEL_11, LL_ADC_CHANNEL_14}, {LL_ADC_CHANNEL_VOPAMP1}, m2006_enc_values, LL_ADC_SAMPLINGTIME_2CYCLES_5);
+    result &= adcs[1].config({}, {LL_ADC_CHANNEL_VOPAMP2, LL_ADC_CHANNEL_VOPAMP3_ADC2}, dummy, LL_ADC_SAMPLINGTIME_2CYCLES_5);
+    serial << "ADCv2 init...\t" << result << "\n";
+
+    result &= canfd.config(peripheral::canfd::Format::fd, peripheral::canfd::Mode::normal, 5e6, 5e6);
+    serial << "CANFD1 init...\t" << result << "\n";
+
+    return true;
+}
+
+}
---
+++ src/board.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "delay.h"
+#include "RUR_STM_G4Lib/version.hpp"
+#include "RUR_STM_G4Lib/Pin.hpp"
+#include "RUR_STM_G4Lib/GPIO.hpp"
+#include "RUR_STM_G4Lib/TIM.hpp"
+#include "RUR_STM_G4Lib/UART.hpp"
+#include "RUR_STM_G4Lib/SPI.hpp"
+#include "RUR_STM_G4Lib/other.hpp"
+#include "RUR_STM_G4Lib/OPAMP.hpp"
+#include "RUR_STM_G4Lib/ADCv2.hpp"
+#include "RUR_STM_G4Lib/CANFD.hpp"
+
+#include "math.h"
+
+
+namespace x610 {
+
+constexpr uint32_t kTimerInterruptFreq = 1000;
+constexpr uint32_t kPWMTimerFreq = 40000;
+
+extern G4::UART serial;
+extern G4::TIM it_timer;
+extern G4::TIM pwm_timer;
+extern std::array<G4::PWM, 3> pwms;
+extern std::array<G4::OpAMP, 3> opamps;
+extern std::array<G4::ADCv2, 2> adcs;
+extern G4::CANFD canfd;
+extern G4::GPIO n_fault;
+extern G4::GPIO drvoff;
+extern G4::LED user_led;
+extern G4::PushSensor user_sw;
+extern G4::PushSensor limit_sw;
+
+extern std::vector<uint16_t> m2006_enc_values;
+
+extern void it_timer_task();
+
+bool config();
+
+}
